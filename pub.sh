082a93af683c595b07feb7214d36d7851b5bdbc2
aeizzz <aeizzz@foxmail.com>
2018-11-13 20:15:02 +0800

    auto publish

--- pub.sh
+++ pub.sh
@@ -2,7 +2,7 @@
 #先提交自己
 git add .
 git commit -am "auto publish"
-git push origin v3
+git push origin master
 #然后编辑发布线上版本
 cross-env NODE_ENV=production webpack --progress --hide-modules
 mkdir publish
@@ -11,12 +11,12 @@ cp ./index.html ./publish/index.html -rf
 cp ./CNAME ./publish/CNAME -rf
 #进入gh-pages发布逻辑
 cd publish
-mv index.html 404.html
+cp index.html 404.html
 echo "" > .gitignore
 git init
 git add .
 git commit -am "publish"
-git push --force --quiet https://github.com/Rozbo/blog.git master:gh-pages
+git push --force --quiet https://github.com/7326/blog.git master:gh-pages
 cd ../
 # 清理资源
 rm -rf ./publish
